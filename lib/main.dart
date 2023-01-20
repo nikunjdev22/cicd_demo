e27d0b4f590b8062e0b443439f7b25fa0eaa70c6
nikunj <nikunjc.itpathsolutions@gmail.com>
2023-01-20 19:02:10 +0530

    [X] DONE :- - Final Last Commit.

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              "CI CD DEMO APPLICATION 1",
+              "TESTING",
             ),
             Text(
               '$_counter',
